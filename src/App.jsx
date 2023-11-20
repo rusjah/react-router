dd1fc32ef1f8fd3bbcc220287ce43991106c8d5b
ruslana_hural <rusjah1@gmail.com>
2023-11-21 00:08:45 +0100

    created header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css';
 
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+
+
+
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className="main">
+       
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+        <p className="logo">Recipes</p>
+    </div>
+  )
+}
+
+export default Header
